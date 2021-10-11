16aa32a2bb78febe6cad95afc7285d5152a1cf51
diwakar <diwakarsingh052@gmail.com>
2021-10-11 21:43:19 +0530

    .

---
+++ quiz/methods/t3.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type files struct {
+	fs *os.File
+}
+
+func (f *files) open(name string) error {
+	var err error
+
+	f.fs, err = os.OpenFile(name, os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (f files) Create(data string) error {
+
+	//fmt.Println("fs", f.fs)
+	_, err := f.fs.Write([]byte(data))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// options
+/*
+a-> Creating err , Writing nil
+b -> Creating nil, Writing nil  //no error in creating file and writing to it
+c -> Creating nil, Writing err
+d-> Creating err, Writing err
+*/
+
+func main() {
+	var f files
+	fmt.Println("creating", f.open("abc.txt"))
+	defer f.fs.Close()
+	fmt.Println("writing", f.Create("hello"))
+	fmt.Println("writing", f.Create("world"))
+
+}
